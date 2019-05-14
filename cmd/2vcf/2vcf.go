15f4d2dceb00abcde7937037ad8127433da79a2d
plantimals <5475390+plantimals@users.noreply.github.com>
2019-05-13 20:42:03 -0500

    remove support for google genomics

--- cmd/2vcf/2vcf.go
+++ cmd/2vcf/2vcf.go
@@ -4,14 +4,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
 	"github.com/briandowns/spinner"
 	"github.com/fatih/color"
 	"github.com/plantimals/2vcf/convert"
-	"github.com/plantimals/2vcf/genomicsuploader"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -19,23 +17,11 @@ import (
 var (
 	app = kingpin.New("2vcf", "convert raw genotype calls from sources like 23andme or ancestry.com into vcf format and upload it to google genomics")
 
-	conv        = app.Command("conv", "convert raw data to vcf format")
-	rawType     = conv.Arg("raw genotype source", "source of the raw genotype data, 23andme or ancestry.com").Required().Enum("23andme", "ancestry")
-	inFile      = conv.Arg("input-data", "relative path to input data, zip or ascii").Required().ExistingFile()
-	outFile     = conv.Flag("output-file", "relative path to output data, gzipped").Short('o').String()
-	vcfRef      = conv.Flag("vcf-ref", "relative path to vcf reference data, gzipped").Default("reference/reference.vcf.gz").Short('v').String()
-	convProject = conv.Flag("google-project", "the google cloud project to push your vcf into").Short('g').String()
-	convBucket  = conv.Flag("bucket", "google cloud storage url to bucket to be used for staging").Short('b').String()
-	convPush    = conv.Flag("push", "push generated VCF file into google genomics").Short('p').Bool()
-
-	push        = app.Command("push", "push a vcf into google genomics")
-	inFilePush  = push.Arg("input-data", "relative path to input data, zip or ascii").Required().ExistingFile()
-	pushProject = push.Flag("google-project", "the google cloud project to push your vcf into").Required().Short('g').String()
-	pushBucket  = push.Flag("bucket", "google cloud storage url to bucket to be used for staging").Required().Short('b').String()
-	dsName      = push.Flag("dataset-name", "name for the dataset to load variants into. if it matches and existing dataset, 2vcf "+
-		"will load your variants into the existing dataset, if it doesn't exist, "+
-		"2vcf will create a dataset by that name").Default("2vcf dataset").Short('d').String()
-	vsName = push.Flag("variantset-name", "name for your variants, will get or create a variantset of this name").Default("2vcf variants").Short('v').String()
+	conv    = app.Command("conv", "convert raw data to vcf format")
+	rawType = conv.Arg("raw genotype source", "source of the raw genotype data, 23andme or ancestry.com").Required().Enum("23andme", "ancestry")
+	inFile  = conv.Arg("input-data", "relative path to input data, zip or ascii").Required().ExistingFile()
+	outFile = conv.Flag("output-file", "relative path to output data, gzipped").Short('o').String()
+	vcfRef  = conv.Flag("vcf-ref", "relative path to vcf reference data, gzipped").Default("reference/reference.vcf.gz").Short('v').String()
 )
 
 var (
@@ -47,8 +33,6 @@ func main() {
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 	case conv.FullCommand():
 		RunConv()
-	case push.FullCommand():
-		RunPush(*inFilePush, *pushProject, *pushBucket, *dsName, *vsName)
 	}
 }
 
@@ -72,23 +56,4 @@ func RunConv() {
 	s.Stop()
 
 	fmt.Printf("\nvcf output at: %s\n\n", cyan(outputFile))
-
-	if *convPush {
-		if *convProject == "" || *convBucket == "" {
-			kingpin.FatalUsage("if --push is used to push the output of conversion into google genomics, a project and bucket must be specified")
-		}
-		RunPush(outputFile, *convProject, *convBucket, "2vcf data", "2vcf variants")
-	}
-}
-
-func RunPush(input string, project string, bucket string, datasetName string, variantsetName string) {
-
-	ggClient, err := genomicsuploader.New(project, bucket)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := ggClient.ImportVCF(input); err != nil {
-		log.Fatal(err)
-	}
 }
